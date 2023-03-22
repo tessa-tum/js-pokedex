4f9bf72b464ba127ff6be766df4d744e1d074b36
tessatum <125835305+tessatumbraegel@users.noreply.github.com>
2023-03-22 02:03:33 +0100

    Added list w/ 6 pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-alert('hello world');
-let favoriteFood = 'Ramen';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'Bulbasaur', height: 0.7, type: ['grass','poison']},
+  {name: 'Ivysaur', height: 1, type: ['grass','poison']},
+  {name: 'Venusaur', height:2, type:['grass','poison']},
+
+  {name: 'Charmander', height:0.6, type:['fire']},
+  {name: 'Charmeleon', height:1.1, type:['fire']},
+  {name: 'Charizard', height:1.7, type:['fire','flying']},
+];
+
